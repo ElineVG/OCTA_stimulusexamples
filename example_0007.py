068b9964b8cd7e94253c92d59e65e8ede29cd8ee
ElineVG <38682338+ElineVG@users.noreply.github.com>
2021-11-03 15:46:37 +0100

    Update example 7

--- example_0007.py
+++ example_0007.py
@@ -7,6 +7,7 @@ created by Eline Van Geert
 # Load necessary objects and functions from OCTA
 from octa.Stimulus import Outline
 from octa.patterns import GridPattern
+from octa.shapes import Rectangle, Triangle, Ellipse
 
 # Define stimulus name
 stimname = "example_0007"
@@ -15,7 +16,16 @@ stimname = "example_0007"
 stim = Outline(n_elements = 24, shape_boundingbox = (100,100), size = (250,250))
 
 # Add bounding box sizes for the elements
-stim.boundingboxes = GridPattern.RepeatAcrossElements( [ (15,15) ] )
+stim.boundingboxes = GridPattern.RepeatAcrossElements( [ (20,20), (15,15) ] )
+
+# Add shapes for the elements
+stim.shapes = GridPattern.RepeatAcrossElements( [ Rectangle, Triangle, Ellipse ] )
+
+# Add fillcolors for the elements
+stim.fillcolors = GridPattern.GradientAcrossElements( start_value = 'limegreen', end_value = 'steelblue' )
+
+# Add orientations for the elements
+stim.orientations = GridPattern.MirrorAcrossElements( [ -90, -45, 0, 45, 90 ] )
 
 # Save stimulus
 stim.Show()
