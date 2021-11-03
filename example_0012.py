eabc8f7b7e8d7d218e0e7c66408d390d4fed1b9a
ElineVG <38682338+ElineVG@users.noreply.github.com>
2021-11-03 15:06:58 +0100

    Example 12

---
+++ example_0012.py
@@ -0,0 +1,34 @@
+"""
+OCTA toolbox example stimuli
+Example stimulus 12
+created by Eline Van Geert
+"""
+
+# Load necessary objects and functions from OCTA
+from octa.Stimulus import Grid
+from octa.Positions import Positions
+from octa.patterns import GridPattern
+from octa.shapes import Ellipse
+
+# Define stimulus name
+stimname = "example_0012"
+
+# Define stimulus
+stim = Grid(n_rows = 9, n_cols = 9, size = (250,250))
+
+# Define element positions
+stim.positions = Positions.CreateSineGrid(n_rows = 9, n_cols = 9, row_spacing = 25, col_spacing = 25,
+                                          A = 15, f = .1, axis = "y")
+
+# Add bounding box sizes for the elements
+stim.boundingboxes = GridPattern.RepeatAcrossElements( [ (15,15) ] )
+
+# Add shapes for the elements
+# stim.shapes = GridPattern.RepeatAcrossElements( [ Ellipse ] )
+
+# Save stimulus
+stim.Show()
+stim.SaveSVG(stimname, folder = "svg")
+stim.SaveJSON(stimname, folder = "json")
+stim.SavePNG(stimname, folder = "png")
+stim.SavePNG(stimname, scale = 10, folder = "png10")
